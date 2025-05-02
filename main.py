0689f66f48407edf58318c653de79165fe6a8142
Suhaib806 <suhaibmalik986@gmail.com>
2025-05-02 16:02:52 +0500

    pdf to jpg added

--- main.py
+++ main.py
@@ -880,4 +880,4 @@ async def convert_pdf_to_jpg(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) 
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)  
